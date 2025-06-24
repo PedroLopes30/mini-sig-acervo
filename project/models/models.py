094df8bdaabbc57b85817eb2f58d6c877cdb235c
PedroLopes30 <pedrohenriquelds1506@gmail.com>
2025-06-24 15:31:20 -0300

    Criação do módulo Emprestimo

--- project/models/models.py
+++ project/models/models.py
@@ -40,4 +40,28 @@ class Usuario(BaseEntity):
         return self.nome.lower() < other.nome.lower()
 
     def __str__(self):
-        return f"{self.nome}"     
+        return f"{self.nome}"  
+
+class Emprestimo(BaseEntity):
+
+    def __init__(self, obra, usuario):
+        self.obra = obra
+        self.usuario = usuario
+        self.data_retirada = datetime.date.today()
+        self.data_prev_devol = None 
+
+    def marcar_devolucao(self, data_dev_real): 
+        self.data_prev_devol = self.data_retirada + datetime.timedelta(days=data_dev_real)
+        return self.data_prev_devol
+
+    def dias_atraso(self, data_ref):
+        if self.data_prev_devol:
+            atraso = (data_ref - self.data_prev_devol).days
+            if atraso > 0:
+                return atraso
+            else:
+                return 0
+        return None
+    
+    def __str__(self):
+        return f"-> {self.obra.titulo} - Data de retirada: {self.data_retirada}\n - Data prevista de devolução: {self.data_prev_devol}\n - Usuário: {self.usuario.nome}"
