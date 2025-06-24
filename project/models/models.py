1fe76a610a3bc5f25b32802215c203d50ef555b4
Pkpkawe <kawepietro50@gmail.com>
2025-06-24 12:15:47 -0300

    Criação base da classe 'Acervo'

--- project/models/models.py
+++ project/models/models.py
@@ -13,3 +13,42 @@ class BaseEntity():
 
     def _gerar_id(self):
         return uuid.uuid4()
+
+class Acervo():
+    def __init__(self):
+        pass
+
+    def __iadd__(self, obra:Obra):
+        pass
+
+    def __isub__(self, obra:Obra):
+        pass
+    
+    def adicionar(self, obra):
+        pass
+
+    def remover(self, obra):
+        pass
+
+    def emprestar(self, obra, usuario, dias=7):
+        pass
+
+    def devolver(self, emprestimo, data_dev):
+        pass
+
+    def renovar(self, emprestimo, dias_extra):
+        pass
+    def relatorio_inventario(self, emprestimo, data_ref):
+        pass
+
+    def relatorio_debitos(self):
+        pass
+
+    def relatorio_usuario(self, usuario):
+        pass
+
+    def _valida_obra(self, obra):
+        pass
+
+    def _relatorio_builder(self, titulo):
+        pass
